80fd20258c1a8ed0483890e1cf0936dcdf78d501
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2019-11-11 17:41:07 -0300

    Trun shouldn't process the reasoners connectors.

--- tests/run_test.fish
+++ tests/run_test.fish
@@ -6,10 +6,12 @@ cd php
 set f (find -name '*test.php')
 set out 0
 for i in $f
-    phpunit --stop-on-error --colors=always --testdox --include ../../ $i
-    if test $status -ne 0
-	set out 1
-    end
+    if test -z (echo "$i" | grep '.*connector.*')
+        phpunit --stop-on-error --colors=always --testdox --include ../../ $i
+        if test $status -ne 0
+	    set out 1
+        end
+    end 
 end
 exit $out
 
