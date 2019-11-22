72ecd950e254b27da6133704ef1144310256b65a
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2019-11-22 19:50:46 -0300

    querying/satisfiable support the v2 and v1 json.

--- querying/satisfiable.php
+++ querying/satisfiable.php
@@ -25,25 +25,55 @@
    Return if the given diagram is satisfiable.
  */
 
-require_once("../../common/import_functions.php");
+require_once("../common/import_functions.php");
 
-load("wicom.php", "../../common/");
+load("v1tov2.php", "../json2json/");
+load("wicom.php", "../common/");
+
+use Json2Json\v1tov2;
+
+if (!array_key_exists('json', $_POST)){
+    echo "{\"error\": \"json parameter not founded.\"}";
+    exit();
+}
+
+/**
+ */
+function convert_v2tov1($json){
+    $conv = new V2toV1($_POST['JSON']);
+    return $conv->convert_str();
+} // convert_v2tov1
+
+
+$json = $_POST['json'];
+if (array_key_exists('json_version', $_GET)){
+    switch ($_GET['json_version']){
+        case "1":
+            $json = $_POST['json'];
+            break;
+        case "2":
+            $json = convert_v2tov1($_POST['json']);
+            break;
+            
+    }
+}
 
 $wicom = new Wicom\Wicom();
 
-if (array_key_exists('json', $_POST)){
-    try{
-        $reasoner = 'Konclude';
-        if (array_key_exists('reasoner', $_POST)){
-            $reasoner = $_POST['reasoner'];
-        }
-        $answer = $wicom->full_reasoning($_POST['json'], $reasoner);
-        echo $answer->to_json();
-    }catch(Exception $e){
-        http_response_code(500);
-        echo $e->getMessage();
+try{
+    $reasoner = 'Konclude';
+    if (array_key_exists('reasoner', $_POST)){
+        $reasoner = $_POST['reasoner'];
     }
-}else{
-    echo "Error, json parameter not founded.";
+    $answer = $wicom->full_reasoning($_POST['json'], $reasoner);
+    if ($answer != null){
+        echo $answer->to_json();
+    }else{
+        echo "{\"error\": \"no answer from the reasoner\"}";
+    }
+}catch(Exception $e){
+    http_response_code(500);
+    echo $e->getMessage();
 }
+
 ?>
