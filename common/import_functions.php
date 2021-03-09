825347fa155ff66f375569f75d0189a571f8508d
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-03-09 16:21:21 -0300

    Deprecating the load() function.

--- common/import_functions.php
+++ common/import_functions.php
@@ -24,33 +24,57 @@
 $current_path = "./";
 
 /**
-   This is a function for loading a module relative to the current file in static context.
+This is a function for loading a module relative to the current file in static 
+context.
 
-   # Problem   
-   There is a problem about paths. When an HTTP is requested on http://localhost/folderA/index.php the current path is the real one matching folderA virtual one, say ./proyect/folderA/index.php.
+# Problem   
+There is a problem about paths. When an HTTP is requested on 
+http://localhost/folderA/index.php the current path is the real one matching folderA
+virtual one, say ./proyect/folderA/index.php.
 
-   If this index.php requires the file ./proyect/folderA/folderB/require1.php , require1.php is a module that uses the folderA path as current directory, not folderB, and it may require something from other folder like ./proyect/folderA/folderC/require2.php so, it will have:
+If this index.php requires the file ./proyect/folderA/folderB/require1.php , 
+require1.php is a module that uses the folderA path as current directory, not
+folderB, and it may require something from other folder like
+./proyect/folderA/folderC/require2.php so, it will have:
 
-   @code{.php}
-   require_once '../folderC/require2.php';
-   @endcode
+@code{.php}
+require_once '../folderC/require2.php';
+@endcode
 
-   This won't work... Because the current directory is the one matching the HTTP request virtual folder: ./proyect/folderA/ .
+This won't work... Because the current directory is the one matching the HTTP request
+virtual folder: ./proyect/folderA/ .
 
-   This is worse when require1.php is required by other PHP web page from other folder: If an HTTP request the http://localhost/index2.php and this index2.php requires ./project/folderA/folderB/require1.php, but require1.php wants the file on `../folderC/require2.php`, wich will points to ./proyect/../folderC/require2.php giving up with an error.
+This is worse when require1.php is required by other PHP web page from other folder:
+If an HTTP request the http://localhost/index2.php and this index2.php requires 
+./project/folderA/folderB/require1.php, but require1.php wants the file on
+`../folderC/require2.php`, wich will points to ./proyect/../folderC/require2.php 
+giving up with an error.
 
-   # Solution
-   A mechanism that concatenates relative paths:
+# Solution
+A mechanism that concatenates relative paths:
 
-   - When ./project/folderA/index.php executes `load("require1.php", "folderB/")` the current path should change to: "./folderB/".
-       - When require1.php executes `load("require2.php", "../folderC/")` the current paht should change to: "./folderB/../folderC/"
-           - When require2.php finish, the current path should be restored to: "./folderB/" deleting what require1.php stored when loading this file.
-       - When require1.php finish, the current path should be restored to : "./" deleting what index.php stored when loading this file.
+- When ./project/folderA/index.php executes `load("require1.php", "folderB/")` the 
+  current path should change to: "./folderB/".
+- When require1.php executes `load("require2.php", "../folderC/")` the current paht
+  should change to: "./folderB/../folderC/"
+- When require2.php finish, the current path should be restored to: "./folderB/"
+  deleting what require1.php stored when loading this file.
+- When require1.php finish, the current path should be restored to : "./" deleting
+  what index.php stored when loading this file.
+
+@param $module [String] The module to load.
+@param $path   [String] The relative path from the current script where is the
+               module to load.
+
+@deprecated use `require_once __DIR__ . '/path_to_script';` instead.
+
+@return nothing.
  */
-function load($module, $path=null){
+function load($module, $path=null)
+{
     global $current_path;
     
-    if ($path != null){
+    if ($path != null) {
         // Store the current path before modifying it.
         $previous_path = $current_path;
         $current_path .= $path;
@@ -62,10 +86,10 @@ function load($module, $path=null){
         require_once($current_path . $module);
         
     }catch(Exception $e){
-        echo "Fatal error:\nCouldn't load in $current_path the module $module\n";        
+        echo "Fatal error:\nCouldn't load in $current_path the module $module\n";
     }
 
-    if ($path != null){
+    if ($path != null) {
         // Restore the current path with the previous.
         $current_path = $previous_path;
     }
