6b2a9928179a39e56c4adc0e45fb6b0b41603a54
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-03-09 16:09:12 -0300

    reasoner: connectors uses require instead of load.

--- wicom/reasoner/koncludeconnector.php
+++ wicom/reasoner/koncludeconnector.php
@@ -23,8 +23,8 @@
 
 namespace Wicom\Reasoner;
 
-load("connector.php");
-load("config.php", "../../config/");
+require_once __DIR__ . '/connector.php';
+require_once __DIR__ . '/../../config/config.php';
 
 use Wicom\Reasoner\Connector;
 use DOMDocument;
